d62a16a5e9c46d5a9cc76eddf764ca130ddbaf98
Yamaguchi Atsushi <s22c1130mt@s.chibakoudai.jp>
2023-11-02 11:24:55 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Yamaguchi Atsushi
-# SPDX-Licence-Identifier: BSD-3-Cause
+# SPDX-Licence-Identifier: BSD-3-Clause
 
 ng () {
 	echo NG at line $1
@@ -11,7 +11,15 @@ res=0
 
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
+[ "${out}" = 15 ]  || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]       || ng ${LINENO}
+[ "${out}" = "" ]  || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]       || ng ${LINENO}
+[ "${out}" = "" ]  || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
-exit $res
+  exit $res
