a90052962203e1dc7c298e8dfd82b58c7e2d3108
Yamaguchi Atsushi <s22c1130mt@s.chibakoudai.jp>
2023-11-25 13:05:02 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash
-
-out=$(seq 5 | ./plus)
-
-[ "${out}" = 15 ]
-=======
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Yamaguchi Atsushi
 # SPDX-Licence-Identifier: BSD-3-Clause
@@ -16,9 +9,8 @@ ng () {
 
 res=0
 
-out=$(seq 5 | ./plus_stdin)
-[ "${out}" = 15.0 ]  || ng ${LINENO}
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ]  || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
->>>>>>> ca86eb85b521ab1527761100ee70b7db2b8fc629
