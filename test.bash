ba0d7e721188a6272b37f331def47d8b61051c2e
Yamaguchi Atsushi <s22c1130mt@s.chibakoudai.jp>
2023-11-25 14:18:43 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -12,5 +12,13 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 14 ]  || ng ${LINENO}
 
+out=$(echo あ | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　
+  out=$(echo | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 exit $res
