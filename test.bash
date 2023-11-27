6d3f0e36d74a439e86155d8922b6f6a5ea88acdb
Yamaguchi Atsushi <s22c1130mt@s.chibakoudai.jp>
2023-11-27 14:05:44 +0900

    Pra test.bash

--- test.bash
+++ test.bash
@@ -10,15 +10,15 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-  [ "${out}" = 14 ]  || ng ${LINENO}
+   [ "${out}" = 15.0 ]  || ng ${LINENO}
 
 out=$(echo あ | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
+   [ "$?" = 1 ]      || ng ${LINENO}
+   [ "${out}" = "" ] || ng ${LINENO}
     　
 out=$(echo | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
+   [ "$?" = 1 ]      || ng ${LINENO}
+   [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
