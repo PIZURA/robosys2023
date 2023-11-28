7062742746c66f41e6cb5244446b0fef8a57af1f
Yamaguchi Atsushi <s22c1130mt@s.chibakoudai.jp>
2023-11-28 18:34:52 +0900

    explain

--- test.bash
+++ test.bash
@@ -3,22 +3,22 @@
 # SPDX-Licence-Identifier: BSD-3-Clause
 
 ng () {
-	echo NG at line $1
-	res=1
+        echo NG at line $1
+        res=1
 }
 
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ]  || ng ${LINENO}
+   [ "${out}" = 15.0 ]  || ng ${LINENO}
 
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+   [ "$?" = 1 ]      || ng ${LINENO}
+   [ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+   [ "$?" = 1 ]      || ng ${LINENO}
+   [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
